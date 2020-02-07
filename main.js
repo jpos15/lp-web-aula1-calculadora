e05809c8ce5bbbecdba3aa0d869432da27fb1f47
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-02-06 21:21:11 -0300

    Adicionado campo histótico

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ function calcular() {
   let x = document.getElementById("resultado").value
   let y = eval(x)
   document.getElementById("resultado").value = y
+
+  document.getElementById("historico").value += (x + ' = ' + y) + '\n'
 }
 
 function limpar() {
   document.getElementById("resultado").value = ""
 } 
+
+function limparHistorico() {
+  document.getElementById("historico").value = ""
+}
